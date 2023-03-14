9acc3a93a80d54b0ff3bde198ba01a7dac8ea8a2
Andrew Shulaev <ash.drone@gmail.com>
2023-03-14 13:30:08 +0000

    Remove unneeded type-level tests

--- client/src/models/project.ts
+++ client/src/models/project.ts
@@ -29,23 +29,3 @@ export type Update = {
   content: Validated<typeof updateValidator>;
   timestamp: number;
 }
-
-// TODO: extract these "tests" into actual tests.
-const test1: Update = {
-  timestamp: 1,
-  content: {
-    kind: "created",
-    description: "Test",
-    isActive: true
-  }
-};
-
-const test2: Update = {
-  timestamp: 2,
-  content: {
-    kind: "update",
-    // TODO: figure out how to get rid of extra "undefined" fields in the literals declaration
-    description: undefined,
-    isActive: undefined
-  }
-};
