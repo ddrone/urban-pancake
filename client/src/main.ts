3e3ac8c66c2a68ed6f751a8a87dc8b527fa60fc1
Andrew Shulaev <ash.drone@gmail.com>
2023-03-15 21:21:37 +0000

    Implement two-column layout

--- client/src/main.ts
+++ client/src/main.ts
@@ -30,54 +30,59 @@ class Main implements m.ClassComponent {
     }
   }
 
-  view(): m.Child {
-    return m('div',
-      this.currentReminder !== undefined && [
-        m('h1', 'Daily reminder'),
-        m('.card',
-          this.reminders[this.currentReminder.index].text
-        )
-      ],
-      m('h1', 'My projects'),
-      this.projects.map(project => m(ProjectEditor, project)),
-      m(TextInput, {
-        buttonText: 'Add project',
-        onEntry: (value) => {
-          this.projects.push({
-            project: createProject(value)
-          });
-        },
-      }),
-      this.newReminders.length > 0 && [
-        m('h1', 'New reminders'),
-        this.newReminders.map(reminder => m('.card', reminder)),
-      ],
-      m(TextInput, {
-        buttonText: 'Add reminder',
-        onEntry: (value) => {
-          this.newReminders.push(value);
-          this.reminders.push({
-            text: value,
-            timesShown: 0
-          });
-        }
-      }),
-      m(Button, {
-        onclick: () => {
-          const state: AppState = {
-            projects: [],
-            reminders: this.reminders,
-            lastReminder: this.currentReminder
+  view(): m.Children {
+    return [
+      m('.column',
+        this.currentReminder !== undefined && [
+          m('h1', 'Daily reminder'),
+          m('.card',
+            this.reminders[this.currentReminder.index].text
+          )
+        ],
+        m('h1', 'My projects'),
+        this.projects.map(project => m(ProjectEditor, project)),
+        m(TextInput, {
+          buttonText: 'Add project',
+          onEntry: (value) => {
+            this.projects.push({
+              project: createProject(value)
+            });
+          },
+        }),
+        this.newReminders.length > 0 && [
+          m('h1', 'New reminders'),
+          this.newReminders.map(reminder => m('.card', reminder)),
+        ],
+        m(TextInput, {
+          buttonText: 'Add reminder',
+          onEntry: (value) => {
+            this.newReminders.push(value);
+            this.reminders.push({
+              text: value,
+              timesShown: 0
+            });
           }
+        }),
+        m(Button, {
+          onclick: () => {
+            const state: AppState = {
+              projects: [],
+              reminders: this.reminders,
+              lastReminder: this.currentReminder
+            }
 
-          for (const p of this.projects) {
-            state.projects.push(p.project);
+            for (const p of this.projects) {
+              state.projects.push(p.project);
+            }
+
+            saveState(state);
           }
-
-          saveState(state);
-        }
-      }, 'Save state')
-    )
+        }, 'Save state')
+      ),
+      m('.column',
+        m('h1', 'Last updates'),
+      )
+    ]
   }
 }
 
