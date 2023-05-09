d233b7f14cbcae3247c0c2b1f4fae39e2b805418
Andrew Shulaev <ash.drone@gmail.com>
2023-05-09 12:41:54 +0100

    Add keyboard shortcut for adding an entry

--- client/src/language/repl.ts
+++ client/src/language/repl.ts
@@ -4,20 +4,31 @@ export class Repl implements m.ClassComponent {
   inputs: string[] = [];
   textarea: HTMLTextAreaElement = undefined as any;
 
+  handleEntry() {
+    this.inputs.push(this.textarea.value);
+    this.textarea.value = '';
+  }
+
   view(): m.Child {
     return m('div',
       m('.entry',
         m('textarea', {
           oncreate: (node: m.VnodeDOM) => {
             this.textarea = node.dom as HTMLTextAreaElement;
+          },
+          onkeyup: (e: KeyboardEvent) => {
+            if (e.key === 'Enter' && e.ctrlKey) {
+              this.handleEntry();
+            }
           }
         }),
         m('br'),
         m('button', 
           {
             onclick: () => {
-              this.inputs.push(this.textarea.value);
-            }
+              this.handleEntry();
+            },
+            title: 'Ctrl+Enter'
           },
           'Enter')),
       this.inputs.slice().reverse().map(input => m('.card', input))
