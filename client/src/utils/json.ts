503df613ac3930940717b4d7f1fa9e86624ad924
Andrew Shulaev <ash.drone@gmail.com>
2023-05-09 12:46:11 +0100

    Add a typed wrapper for parsing JSON

--- client/src/utils/json.ts
+++ client/src/utils/json.ts
@@ -7,3 +7,12 @@ type JsonArray = Array<Json>;
 type JsonObject = {
   [k in string]: Json
 };
+
+function readJson(input: string): Json|undefined {
+  try {
+    return JSON.parse(input) as Json;
+  }
+  catch {
+    return undefined;
+  }
+}
