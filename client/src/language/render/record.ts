7d45aa7c09ae17d51736f442c5c03d4e550a7358
Andrew Shulaev <ash.drone@gmail.com>
2023-05-14 13:04:01 +0100

    First version of printing records

--- client/src/language/render/record.ts
+++ client/src/language/render/record.ts
@@ -1,7 +1,7 @@
 import m from 'mithril';
 import { Type } from '../types';
 import { Json, JsonObject } from '../../utils/json';
-import { concatPrefix } from './array_of_records';
+import { concatPrefix, printCellValue } from './array_of_records';
 import { onlyMapEntry } from '../../utils/map';
 
 function innermost(type: Type, name: string, value: Json): [Type, string, Json] {
@@ -14,20 +14,42 @@ function innermost(type: Type, name: string, value: Json): [Type, string, Json] 
 }
 
 export function renderRecord(items: Map<string, Type>, value: JsonObject): m.Child[] {
-  const renderedRows: m.Child[][] = [
-    ['To be done']
-  ];
+  const renderedRows: m.Child[] = [];
 
   renderRecordToRows(renderedRows, 0, items, value);
 
-  return renderedRows.map(row => m('tr', row));
+  return renderedRows;
 }
 
-function renderRecordToRows(rows: m.Child[][], indent: number, items: Map<string, Type>, value: JsonObject) {
+function renderRecordToRows(rows: m.Child[], indent: number, items: Map<string, Type>, value: JsonObject) {
   for (const [fieldName, fieldType] of items.entries()) {
     // TODO: This kind of pattern of value[fieldName] should be abstracted away with runtime type checking
     const [childType, key, child] = innermost(fieldType, fieldName, value[fieldName]);
+    const style = {
+      'padding-left': `${0.5 * (indent + 1)}em`
+    };
 
-    // TODO: continue from here
+    if (childType.kind === 'record') {
+      rows.push(m('tr', m('td', {
+        colspan: 2,
+        style
+      }, key)));
+
+      renderRecordToRows(rows, indent + 1, childType.items, child as JsonObject);
+      continue;
+    }
+
+    const cells = printCellValue(childType, child);
+
+    rows.push(m('tr',
+      m('td', {
+        rowspan: cells.length,
+        style
+      }, key),
+      m('td', cells[0])
+    ))
+    for (let i = 1; i < cells.length; i++) {
+      rows.push(m('tr', m('td', cells[i])));
+    }
   }
 }
